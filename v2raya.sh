7c056c8f53262582bec6d9a7135750de34aeb401
Kyaw Swar Thwin <kyawswar.dev@gmail.com>
2024-10-13 04:19:12 +0630

    Update v2raya.sh

--- v2raya.sh
+++ v2raya.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# Helper function to install packages
-install_pkg() {
-  if ! opkg list-installed | grep -q "$1"; then
-    opkg install "$1"
-  fi
-}
-
 echo "Adding v2rayA signing key..."
-install_pkg wget-ssl
 wget https://downloads.sourceforge.net/project/v2raya/openwrt/v2raya.pub -O /etc/opkg/keys/94cc2a834fb0aa03
 
 echo "Importing v2rayA feed..."
@@ -18,33 +10,33 @@ echo "Updating opkg feeds..."
 opkg update
 
 echo "Installing v2rayA..."
-install_pkg v2raya
+opkg install v2raya
 
 echo "Installing firewall packages..."
 install_firewall_packages() {
   if command -v fw4 > /dev/null 2>&1; then
-    install_pkg kmod-nft-tproxy
+    opkg install kmod-nft-tproxy
   elif command -v fw3 > /dev/null 2>&1; then
-    install_pkg iptables-mod-conntrack-extra
-    install_pkg iptables-mod-extra
-    install_pkg iptables-mod-filter
-    install_pkg iptables-mod-tproxy
-    install_pkg kmod-ipt-nat6
+    opkg install iptables-mod-conntrack-extra
+    opkg install iptables-mod-extra
+    opkg install iptables-mod-filter
+    opkg install iptables-mod-tproxy
+    opkg install kmod-ipt-nat6
   fi
 }
 install_firewall_packages
 
 echo "Installing Xray core..."
-install_pkg xray-core
+opkg install xray-core
 
 # Optional: Uncomment to install geoip and geosite databases
 # echo "Installing geoip and geosite databases..."
-# install_pkg v2fly-geoip
-# install_pkg v2fly-geosite
+# opkg install v2fly-geoip
+# opkg install v2fly-geosite
 
 if opkg list-installed | grep -q 'luci'; then
   echo "Installing LuCI app for v2raya..."
-  install_pkg luci-app-v2raya
+  opkg install luci-app-v2raya
 fi
 
 echo "Enabling and starting v2rayA service..."
